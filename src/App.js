cd8b5846b66e56cac274c76495da29cd5ab1f9d8
galibkhan1 <vampboy876@gmail.com>
2024-08-04 16:48:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { TodoWrappers } from './component/TodoWrappers';
+function App() {
+  return (
+    <div className="App">
+     <TodoWrappers />
+    </div>
+  );
+}
+
+export default App;
